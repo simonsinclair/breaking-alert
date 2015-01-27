51673d7e4c34e3f8062dea6da6d1ec5144057187
Simon Sinclair <simon@sinclair>
2015-01-27 16:24:40 +0000

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,37 @@
 (function(w, $, undefined) {
   "use strict";
 
-	var Module = {
+	var Breaking = {
+
+    visible: false,
 
     init: function() {
-      // ...
+      Breaking.showAlertIn(3000);
+
+      $('#js-breaking-close').on('click', function(e) {
+        e.preventDefault();
+
+        if(Breaking.visible) {
+          Breaking.hideAlert();
+          Breaking.showAlertIn(4000);
+        }
+      });
+    },
+
+    showAlertIn: function(ms) {
+      setTimeout(Breaking.showAlert, ms);
+    },
+
+    showAlert: function() {
+      $("#js-breaking").addClass('breaking--visible');
+      Breaking.visible = true;
+    },
+
+    hideAlert: function() {
+      $("#js-breaking").removeClass('breaking--visible');
+      Breaking.visible = false;
     }
   };
 
-	$(Module.init);
+	$(Breaking.init);
 })(this, jQuery);
